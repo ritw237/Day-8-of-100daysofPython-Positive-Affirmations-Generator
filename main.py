ec246fa56ed9f2e15c2ae9a000293ca31011799e
Ritwik Srivastava <ritwikdbz@gmail.com>
2022-10-26 16:11:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Positive Affirmations Generator!!!")
+print()
+name = input("Hello there! What's your name? ")
+print()
+day = input("What's the day today? ")
+print()
+if name =="Ritwik" or name=="ritwik":
+  if day=="Monday" or day=="Tuesday" or day=="Wednesday" or day=="Thursday" or day=="Friday" or day=="Saturday" or day=="Sunday" or day=="monday" or day=="Tuesday" or day=="Wednesday" or day=="Thursday" or day=="Friday" or day=="Saturday" or day=="Sunday":
+
+    print(name,"You got this! Just hang in there!")
+    
+else:
+  print(name,"You will be fine anyways!")
